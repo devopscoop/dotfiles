3a9393f4d1df0b1abb4bf2faf62c14c15bc3dcdc
3uzbcqje <evans.tucker@dfinity.org>
2026-03-26 11:32:43 -0700

    Updating script

--- 3uzbcqje/bin/install_pre-commit.sh
+++ 3uzbcqje/bin/install_pre-commit.sh
@@ -5,12 +5,12 @@ while read -r d; do
   echo -e "\n# ${git_dir}"
   cd "${git_dir}"
   rm .pre-commit-config.yaml
-  ln -s ~/codeberg.org/devopscoop/dotfiles/nxatdo/.pre-commit-config.yaml
+  ln -s ~/github.com/devopscoop/dotfiles/3uzbcqje/.pre-commit-config.yaml
   grep -q .pre-commit-config.yaml .gitignore || echo '.pre-commit-config.yaml' >> .gitignore
-  git add -A
-  git status
-  git commit -m ...
-  git pull-all-main
-  git push-all-branches
+  # git add -A
+  # git status
+  # git commit -m ...
+  # git pull-all-main
+  # git push-all-branches
   pre-commit install
-done < <(find ~/codeberg.org/ ~/gitlab.com/ ~/github.com/ -type d -name ".terraform" -prune -o -type d -name ".git" -print)
+done < <(find ~/codeberg.org/ ~/gitlab.com/ ~/github.com/ ~/git/ ~/aur.archlinux.org/ -type d -name ".terraform" -prune -o -type d -name ".git" -print)
